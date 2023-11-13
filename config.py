15451c5c1dc1c2439d90ea3667514799b67720df
javier <javier.mtgt@gmail.com>
2023-11-13 17:45:24 -0600

    environ get command

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ import os
 #instanciar servidor
 app = Flask(__name__)
 
-MY_EMAIL = os.environ['EMAIL_REMITENTE']
-MY_PASSWORD = os.environ['EMAIL_PASSWORD']
-CONFIG_DESTINATARIO = os.environ['CONFIG_DESTINATARIO']
+MY_EMAIL = os.environ.get('EMAIL_REMITENTE')
+MY_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+CONFIG_DESTINATARIO = os.environ.get('CONFIG_DESTINATARIO')
